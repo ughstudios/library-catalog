2def02955d1034ef043b409e60e83fe5d188b7d3
Daniel <danielpgleason@icloud.com>
2024-07-18 16:50:12 -0700

    [FEATURE] Switch database backend from SQLite to PostgreSQL
    - Added psycopg2 to project dependencies.
    - Updated Django settings to configure PostgreSQL as the database backend using environment variables.

--- src/thechannelcompany/settings.py
+++ src/thechannelcompany/settings.py
@@ -62,8 +62,12 @@ WSGI_APPLICATION = 'thechannelcompany.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PG_DB_NAME'),
+        'USER': os.getenv('PG_USERNAME'),
+        'PASSWORD': os.getenv('PG_PASSWORD'),
+        'HOST': os.getenv('PG_HOST'),
+        'PORT': os.getenv('PG_PORT'),
     }
 }
 
